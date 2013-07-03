b2abb20c15e6f3b18bbd13eddff37d719254e0ed
Franco Catena <francocatena@gmail.com>
2013-07-03 20:51:14 -0300

    User validation moved to concern

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,10 @@
 class User < ActiveRecord::Base
   include Attributes::Strip
   include Attributes::Downcase
+  include Users::Validation
 
   strip_fields :name, :lastname, :email
   downcase_fields :email
 
   has_secure_password
-
-  validates :name, :lastname, presence: true
-  validates :email,
-    uniqueness: { case_sensitive: false },
-    presence: true,
-    format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i }
 end
