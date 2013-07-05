dd35d17100ef43a966ca5450250c2fdb72eb6118
Franco Catena <francocatena@gmail.com>
2013-07-05 16:51:06 -0300

    Menu items in helper now...

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,4 +10,11 @@ module ApplicationHelper
   def flash_class
     flash[:alert] ? 'alert-danger' : 'alert-info'
   end
+
+  def menu_item_for model, path
+    link = link_to model.model_name.human(count: 0), path
+    active = model.model_name.route_key == controller_name
+
+    content_tag(:li, link, (active ? { class: 'active' } : {}))
+  end
 end
--- test/helpers/application_helper_test.rb
+++ test/helpers/application_helper_test.rb
@@ -28,4 +28,10 @@ class ApplicationHelperTest < ActionView::TestCase
 
     assert_equal 'alert-danger', flash_class
   end
+
+  test 'menu item for' do
+    link = link_to User.model_name.human(count: 0), users_path
+
+    assert_equal content_tag(:li, link), menu_item_for(User, users_path)
+  end
 end
