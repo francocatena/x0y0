8aa3e7ede6fb747977e0c7dc2360ac9f071aa436
Franco Catena <francocatena@gmail.com>
2013-07-18 14:44:56 -0300

    Mini fix: current_user search _relaxed_

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   private
 
   def current_user
-    @current_user ||= User.find_by!(auth_token: cookies[:auth_token]) if cookies[:auth_token]
+    @current_user ||= User.find_by(auth_token: cookies[:auth_token]) if cookies[:auth_token]
   end
   helper_method :current_user
 
