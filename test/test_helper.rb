fc3b2a8b63acfa6069df394b0e291f2cb5d3f691
Franco Catena <francocatena@gmail.com>
2014-01-04 15:33:07 -0300

    Test helper cleanup

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,4 +1,5 @@
-ENV["RAILS_ENV"] ||= "test"
+ENV['RAILS_ENV'] ||= 'test'
+
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 require 'minitest/pride'
@@ -6,14 +7,9 @@ require 'minitest/pride'
 class ActiveSupport::TestCase
   ActiveRecord::Migration.check_pending!
 
-  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
-  #
-  # Note: You'll currently still have to declare fixtures explicitly in integration tests
-  # -- they do not yet inherit this setting
   fixtures :all
 
-  # Add more helper methods to be used by all tests here...
-  def assert_error(model, attribute, type, options = {})
+  def assert_error model, attribute, type, options = {}
     assert model.errors[attribute].include?(
       model.errors.generate_message(attribute, type, options)
     )
