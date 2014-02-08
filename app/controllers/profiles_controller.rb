a298722fa2e295872b76606dd8f76d21655ab76c
Franco Catena <francocatena@gmail.com>
2014-02-08 12:48:09 -0300

    Replace respond_to in profiles controller

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,22 +1,17 @@
 class ProfilesController < ApplicationController
+  respond_to :html, :json
+
   before_action :authorize, :set_user
 
   def edit
-    @title = t('.title')
+    @title = t '.title'
   end
 
   def update
-    @title = t('profiles.edit.title')
+    @title = t 'profiles.edit.title'
 
-    respond_to do |format|
-      if @user.update(user_params)
-        format.html { redirect_to root_url, notice: t('.success') }
-        format.json { head :no_content }
-      else
-        format.html { render action: 'edit' }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
-    end
+    @user.update user_params
+    respond_with @user, location: root_url
   end
 
   private
