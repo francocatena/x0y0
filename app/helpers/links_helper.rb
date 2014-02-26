9ae9497f135f7ebc7d322cf00ddfea634f3290c0
Franco Catena <francocatena@gmail.com>
2014-02-26 18:40:13 -0300

    Accept title in link_with_icon helper

--- app/helpers/links_helper.rb
+++ app/helpers/links_helper.rb
@@ -9,34 +9,32 @@ module LinksHelper
     link_with_icon({ action: 'destroy', icon: 'glyphicon-trash' }, *(args << options))
   end
 
-  def link_to_edit(*args)
+  def link_to_edit *args
     link_with_icon({ action: 'edit', icon: 'glyphicon-pencil' }, *args)
   end
 
-  def link_to_index(*args)
+  def link_to_index *args
     link_to t('navigation.index'), *args
   end
 
-  def link_to_new(*args)
+  def link_to_new *args
     link_to t('.new'), *args
   end
 
-  def link_to_show(*args)
+  def link_to_show *args
     link_with_icon({ action: 'show', icon: 'glyphicon-search' }, *args)
   end
 
   private
 
-  def link_with_icon(options = {}, *args)
-    arg_options = args.extract_options!
+    def link_with_icon(options = {}, *args)
+      arg_options = args.extract_options!
+      title = options.fetch(:title) { t("navigation.#{options.fetch(:action)}") }
 
-    arg_options.reverse_merge!(
-      title: t("navigation.#{options.fetch(:action)}"),
-      class: 'icon'
-    )
+      arg_options.reverse_merge! title: title, class: 'icon'
 
-    link_to *args, arg_options do
-      content_tag(:span, nil, class: "glyphicon #{options.fetch(:icon)}")
+      link_to *args, arg_options do
+        content_tag :span, nil, class: "glyphicon #{options.fetch(:icon)}"
+      end
     end
-  end
 end
