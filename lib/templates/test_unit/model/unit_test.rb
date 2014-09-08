bc7f1b846b3365dcbbbe0c953792f36eb421e43a
Franco Catena <francocatena@gmail.com>
2014-09-08 13:51:36 -0300

    More _accurate_ model test template

--- lib/templates/test_unit/model/unit_test.rb
+++ lib/templates/test_unit/model/unit_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 <% module_namespacing do -%>
 class <%= class_name %>Test < ActiveSupport::TestCase
   def setup
-    @<%= singular_table_name %> = <%= table_name %>(:one)
+    @<%= singular_table_name %> = <%= table_name %> :one
   end
 
   test 'blank attributes' do
@@ -14,10 +14,10 @@ class <%= class_name %>Test < ActiveSupport::TestCase
   end
 
   test 'unique attributes' do
-    @<%= singular_table_name %>.attr = <%= table_name %>(:two).attr
+    <%= singular_table_name %> = @<%= singular_table_name %>.dup
 
-    assert @<%= singular_table_name %>.invalid?
-    assert_error @<%= singular_table_name %>, :attr, :taken
+    assert <%= singular_table_name %>.invalid?
+    assert_error <%= singular_table_name %>, :attr, :taken
   end
 end
 <% end -%>
